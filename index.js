175a9aee4de107a21089e5fbd39159cd1facb35d
SathwikGit <f2021567@hyderabad.bits-pilani.ac.in>
2025-09-08 13:03:12 +0530

    Retrieving pokemons done

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let count = 0;
 let type = "NONE";
+
+let pokemons = [];
+
 function takeinput() {
   document.body
     .querySelector(".container")
@@ -14,6 +17,63 @@ function takeinput() {
     .querySelector(".display")
     .appendChild(el);
 
+  fetch("https://pokeapi.co/api/v2/type/" + `${type}`)
+    .then((response) => response.json())
+    .then((data) => {
+      pokemons = data.pokemon;
+      // console.log(electricPokemonArray);
+      addpokemons(count);
+    });
+
   console.log(type);
   console.log(count);
+}
+
+async function createcard(ind) {
+  const el1 = document.createElement("div");
+  el1.className = "card";
+
+  const name = document.createElement("div");
+  name.innerHTML = "NAME: " + `${pokemons[ind].pokemon.name}`;
+
+  let url = pokemons[ind].pokemon.url;
+
+  const response = await fetch(url);
+  const data = await response.json();
+  const imgsrc = data.sprites.front_default;
+
+  const img = document.createElement("img");
+  img.src = imgsrc;
+
+  const health = document.createElement("div");
+  health.innerHTML = data.stats[0].base_stat;
+
+  const select = document.createElement("button");
+  select.innerHTML = "SELECT";
+
+  const del = document.createElement("button");
+  del.innerHTML = "DELETE";
+
+  el1.appendChild(name);
+  el1.appendChild(img);
+  el1.appendChild(health);
+  el1.appendChild(select);
+  el1.appendChild(del);
+
+  return el1;
+}
+
+async function addpokemons(count) {
+  const head = document.createElement("div");
+  head.id = "cards";
+
+  for (let i = 0; i < count; i++) {
+    const el = await createcard(i);
+    head.appendChild(el);
+  }
+
+  document.body
+    .querySelector(".container")
+    .querySelector(".display")
+    .appendChild(head);
 }
